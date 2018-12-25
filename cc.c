7ae70e9bb5e2ca1520e4d977659393dafe615182
Inazuma110 <c011703534@edu.teu.ac.jp>
2018-12-25 23:49:59 +0900

    add: mul and div ()

--- cc.c
+++ cc.c
@@ -3,9 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 enum{
   TK_NUM = 256,
   TK_EOF,
+};
+
+enum {
+  ND_NUM = 256,
 };
 
 typedef struct {
@@ -14,7 +19,21 @@ typedef struct {
   char *input;
 } Token;
 
+typedef struct Node{
+  int ty;
+  struct Node *lhs;
+  struct Node *rhs;
+  int val;
+} Node;
+
+Node* expr();
 Token tokens[100];
+int pos = 0;
+
+void error(int i){
+  fprintf(stderr, "Invalid token: %s\n", tokens[i].input);
+  exit(1);
+}
 
 void tokenize(char *p) {
   int i = 0;
@@ -24,13 +43,14 @@ void tokenize(char *p) {
       continue;
     }
 
-    if(*p == '+' || *p == '-'){
+    if(*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')'){
       tokens[i].ty = *p;
       tokens[i].input = p;
       i++;
       p++;
       continue;
     }
+
 
     if(isdigit(*p)){
       tokens[i].ty = TK_NUM;
@@ -47,10 +67,94 @@ void tokenize(char *p) {
   tokens[i].input = p;
 }
 
-void error(int i ){
-  fprintf(stderr, "Invalid token: %s\n", tokens[i].input);
-  exit(1);
+Node *new_node(int ty, Node *lhs, Node *rhs){
+  Node *node = malloc(sizeof(Node));
+  node->ty = ty;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
 }
+
+Node *new_node_num(int val){
+  Node *node = malloc(sizeof(Node));
+  node->ty = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+
+Node *term(){
+  if(tokens[pos].ty == TK_NUM)
+    return new_node_num(tokens[pos++].val);
+  if(tokens[pos].ty == '(')
+  {
+    pos++;
+    Node *node = expr();
+    if(tokens[pos].ty != ')')
+      error(tokens[pos].input);
+    pos++;
+    return node;
+  }
+  error(tokens[pos].input);
+}
+
+Node *mul(){
+  Node *lhs = term();
+  if(tokens[pos].ty == '*'){
+    pos++;
+    return new_node('*', lhs, mul());
+  }
+  if(tokens[pos].ty == '/'){
+    pos++;
+    return new_node('/', lhs, mul());
+  }
+  return lhs;
+}
+
+Node *expr() {
+  Node *lhs = mul();
+  if(tokens[pos].ty == '+'){
+    pos++;
+    return new_node('+', lhs, expr());
+  }
+  if(tokens[pos].ty == '-'){
+    pos++;
+    return new_node('-', lhs, expr());
+  }
+  return lhs;
+}
+
+void gen(Node *node){
+  if(node->ty == ND_NUM){
+    printf("  push %d\n",node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->ty) {
+    case '+':
+      printf("  add rax, rdi\n");
+      break;
+    case '-':
+      printf("  sub rax, rdi\n");
+      break;
+    case '*':
+      printf("  mul rdi\n");
+      break;
+    case '/':
+      printf("  mov rdx, 0\n");
+      printf("  div rdi\n");
+      break;
+  }
+
+  printf("  push rax\n");
+}
+
 
 int main(int argc, char const* argv[])
 {
@@ -61,38 +165,43 @@ int main(int argc, char const* argv[])
   }
 
   tokenize(argv[1]);
+  Node* node = expr();
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  if(tokens[0].ty != TK_NUM)
-    error(0);
-  printf("  mov rax, %d\n", tokens[0].val);
-  int i = 1;
+  gen(node);
 
-  while(tokens[i].ty != TK_EOF)
-  {
-    if(tokens[i].ty == '+'){
-      i++;
-      if(tokens[i].ty != TK_NUM) error(i);
-      printf("  add rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
+  //
+  // if(tokens[0].ty != TK_NUM)
+  //   error(0);
+  // printf("  mov rax, %d\n", tokens[0].val);
+  // int i = 1;
+  //
+  // while(tokens[i].ty != TK_EOF)
+  // {
+  //   if(tokens[i].ty == '+'){
+  //     i++;
+  //     if(tokens[i].ty != TK_NUM) error(i);
+  //     printf("  add rax, %d\n", tokens[i].val);
+  //     i++;
+  //     continue;
+  //   }
+  //
+  //   if(tokens[i].ty == '-')
+  //   {
+  //     i++;
+  //     if(tokens[i].ty != TK_NUM) error(i);
+  //     printf("  sub rax, %d\n", tokens[i].val);
+  //     i++;
+  //     continue;
+  //   }
+  //
+  //   error(i);
+  // }
 
-    if(tokens[i].ty == '-')
-    {
-      i++;
-      if(tokens[i].ty != TK_NUM) error(i);
-      printf("  sub rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-
-    error(i);
-  }
-
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
